ca68e81337321fca3cb4b78c218c6cbef92d2821
Mike Rosengrant <mike@rosengrant.me>
2018-04-16 15:08:11 -0400

    TDD

--- search/tests.py
+++ search/tests.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class SearchIndexViewTestCase(TestCase):
+    """Test the app's main search functionality."""
+
+    def setUp(self, *args, **kwargs):
+        self.client = Client()
+        self.url = reverse('search:index')
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        return True
